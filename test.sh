84aa82cd340369dbf6150191917d21057930926c
The Fern <krede2000@gmail.com>
2022-11-14 23:12:11 +0100

    Add unit test Add key generation to workflow

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-./vendor/bin/sail up -d
+sleep 10
+
+./vendor/bin/sail artisan migrate:fresh
 
 sleep 10
 
-./vendor/bin/sail artisan migrate:fresh --force
-
-sleep 10
-
-./vendor/bin/sail stop
+./vendor/bin/sail artisan test --recreate-databases
